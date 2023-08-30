e359e847cbd505f3e281d2acc518ed41913f404c
Djmarques18 <derrickmarques18@gmail.com>
2023-08-30 12:44:00 -0400

    Completed

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const grid = document.querySelector(".connect-four");
 const rows = 6;
 const cols = 7;
-let currentPlayer = 1; // Player 1 starts
+let currentPlayer = 1;
+let isGameLocked = false;
 
-// Create the game grid
+
 for (let row = 0; row < rows; row++) {
     for (let col = 0; col < cols; col++) {
         const cell = document.createElement("div");
@@ -14,28 +15,38 @@ for (let row = 0; row < rows; row++) {
     }
 }
 
-// Add event listener for cell clicks
+
 grid.addEventListener("click", handleCellClick);
+
 
 function handleCellClick(event) {
     const clickedCell = event.target;
     const col = clickedCell.dataset.col;
 
-    // Find the bottom-most empty cell in the clicked column
+
     const emptyCell = findEmptyCell(col);
 
     if (emptyCell !== null) {
-        // Place player's piece in the empty cell
+
         emptyCell.classList.add(`player${currentPlayer}`);
-        
-        // Check for a win or draw
+
+
         if (checkWin(emptyCell) || checkDraw()) {
-            // Handle win/draw here
+
         } else {
-            // Switch to the next player
+
             currentPlayer = currentPlayer === 1 ? 2 : 1;
         }
     }
+}
+
+function resetGame() {
+    isGameLocked = false;
+    currentPlayer = 1;
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.classList.remove("player1", "player2", "clicked");
+    });
 }
 
 function findEmptyCell(col) {
@@ -45,15 +56,188 @@ function findEmptyCell(col) {
             return cells[i];
         }
     }
-    return null; // Column is full
+    return null;
 }
 
 function checkWin(cell) {
-    // Implement win checking logic here
-    return false; // Return true if a win is detected
+
+    return false;
 }
 
 function checkDraw() {
-    // Implement draw checking logic here
-    return false; // Return true if the game is a draw
+
+    return false;
 }
+
+
+function checkWin(cell) {
+    const row = parseInt(cell.dataset.row);
+    const col = parseInt(cell.dataset.col);
+    const currentPlayerClass = `player${currentPlayer}`;
+
+
+    let consecutiveCount = 0;
+    for (let i = 0; i < cols; i++) {
+        const cell = document.querySelector(`[data-row="${row}"][data-col="${i}"]`);
+        if (cell.classList.contains(currentPlayerClass)) {
+            consecutiveCount++;
+            if (consecutiveCount === 4) {
+                return true;
+            }
+        } else {
+            consecutiveCount = 0;
+        }
+    }
+
+
+    consecutiveCount = 0;
+    for (let i = 0; i < rows; i++) {
+        const cell = document.querySelector(`[data-row="${i}"][data-col="${col}"]`);
+        if (cell.classList.contains(currentPlayerClass)) {
+            consecutiveCount++;
+            if (consecutiveCount === 4) {
+                return true;
+            }
+        } else {
+            consecutiveCount = 0;
+        }
+    }
+
+
+    consecutiveCount = 0;
+    for (let i = -3; i <= 3; i++) {
+        const newRow = row + i;
+        const newCol = col + i;
+        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+            const cell = document.querySelector(`[data-row="${newRow}"][data-col="${newCol}"]`);
+            if (cell.classList.contains(currentPlayerClass)) {
+                consecutiveCount++;
+                if (consecutiveCount === 4) {
+                    return true;
+                }
+            } else {
+                consecutiveCount = 0;
+            }
+        }
+    }
+
+
+    consecutiveCount = 0;
+    for (let i = -3; i <= 3; i++) {
+        const newRow = row + i;
+        const newCol = col - i;
+        if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+            const cell = document.querySelector(`[data-row="${newRow}"][data-col="${newCol}"]`);
+            if (cell.classList.contains(currentPlayerClass)) {
+                consecutiveCount++;
+                if (consecutiveCount === 4) {
+                    return true;
+                }
+            } else {
+                consecutiveCount = 0;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+function handleCellClick(event) {
+    if (isGameLocked) {
+        return;
+    }
+
+    const clickedCell = event.target;
+    const col = clickedCell.dataset.col;
+
+    const emptyCell = findEmptyCell(col);
+
+    if (emptyCell !== null) {
+        const currentPlayerClass = `player${currentPlayer}`;
+        emptyCell.classList.add(currentPlayerClass, "clicked");
+
+        if (checkWin(emptyCell)) {
+            displayWinPopup(currentPlayer === 1 ? "Red" : "Yellow");
+        } else if (checkDraw()) {
+
+        } else {
+            currentPlayer = currentPlayer === 1 ? 2 : 1;
+        }
+    }
+}
+
+
+function displayWinPopup(winningColor) {
+    const popup = document.createElement("div");
+    popup.classList.add("popup");
+    popup.innerHTML = `
+        <p class="blink-text">Congrats, ${winningColor} won the game!</p>
+        <button class="play-again">Play Again</button>
+        <button id="close-popup">Close</button>
+        <p class="popup-footer">&copy; 2023 Derrick Marques. All rights reserved. | <a href="https://github.com/Djmarques18" target="_blank">GitHub</a></p>
+    `;
+
+    document.body.appendChild(popup);
+    grid.removeEventListener("click", handleCellClick);
+
+    const playAgainButton = popup.querySelector(".play-again");
+    playAgainButton.addEventListener("click", () => {
+        window.location.href = "frontpage.html";
+    });
+
+    const closePopupButton = popup.querySelector("#close-popup");
+    closePopupButton.addEventListener("click", () => {
+        popup.remove();
+        isPopupDisplayed = false;
+        grid.addEventListener("click", handleCellClick);
+        isGameLocked = true;
+
+
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => {
+            cell.classList.remove("hoverable");
+        });
+    });
+}
+
+
+function restartGame() {
+    const popup = document.querySelector(".popup");
+    if (popup) {
+        popup.remove();
+    }
+
+
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.classList.remove("player1", "player2", "clicked");
+    });
+
+
+    currentPlayer = 1;
+    isPlayerTurn = true;
+
+
+
+    cells.forEach(cell => {
+        cell.classList.add("hoverable");
+    });
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const goHomeButton = document.getElementById("go-home");
+    const restartButton = document.getElementById("restart");
+
+    goHomeButton.addEventListener("click", () => {
+        window.location.href = "frontpage.html";
+    });
+
+
+    restartButton.addEventListener("click", () => {
+        resetGame();
+        isGameLocked = false;
+    });
+});
+
